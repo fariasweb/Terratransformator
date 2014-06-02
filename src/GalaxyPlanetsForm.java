c90f3ed9df411bb1c9c530a72716538f1d60c67c
Francisco Javier Arias <fariasweb@gmail.com>
2014-06-02 18:01:03 +0200

    Realcion planeta - galaxia por hacer - DA FALLOSjavac -g *.java && java Terratransformator

---
+++ src/GalaxyPlanetsForm.java
@@ -0,0 +1,85 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.GroupLayout;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle;
+import javax.swing.JButton;
+import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.table.DefaultTableModel;
+
+public class GalaxyPlanetsForm extends ViewForm {
+
+	private JComboBox tplanets;
+	private String[] list;
+
+	/**
+	 * 
+	 * @param gcf
+	 */
+	GalaxyPlanetsForm(GalaxyControllerView gcf) {
+		super(gcf);
+
+	}
+
+	/**
+	 * 
+	 */
+	protected void create_view() {
+
+		JLabel name = new JLabel("Planets: ");
+		tplanets = new JComboBox();
+
+		// Creamos un Layout para colocar Labels y TextFields del formulario
+
+		GroupLayout layout = new GroupLayout(this);
+		this.setLayout(layout);
+		layout.setAutoCreateGaps(true);
+		layout.setAutoCreateContainerGaps(true);
+
+		layout.setVerticalGroup(layout.createParallelGroup().addGroup(
+				layout.createSequentialGroup().addComponent(name)
+						.addComponent(tplanets))
+
+		);
+
+		layout.setHorizontalGroup(layout.createParallelGroup()
+				.addGroup(layout.createSequentialGroup().addComponent(name))
+				.addGroup(layout.createSequentialGroup().addComponent(tplanets))
+		);
+	}
+
+	/**
+	 * 
+	 */
+	protected void create_events() {
+	}
+
+	/**
+	 * 
+	 */
+	public void submit_form() throws Exception {
+
+		// Creacion del objeto por parte del controlador
+		((GalaxyControllerView) controller).addPlanet(tplanets.getSelectedIndex());
+	}
+
+	// --------------------------------------------------------
+	// INPUTS
+	// --------------------------------------------------------
+
+	/**
+	 * 
+	 * @param e
+	 */
+	public void append(String[] e) {
+		//tfname.setText(e);
+		list = e;
+		tplanets.addItem(list);
+	}
+
+}
