f35ed262ebbd11f27c4cdeb6a5eb5d0e207e8456
Manish Thani <manish.thani@gmail.com>
2014-06-06 13:51:44 +0200

    Creando lista de Nodos

--- src/QAPOutputView.java
+++ src/QAPOutputView.java
@@ -21,18 +21,7 @@ import javax.swing.SpinnerNumberModel;
 
 
 public class QAPOutputView extends ViewForm {
-	private JButton inputButton;
-	private JComboBox gcb;
-	private JRadioButton rb1;
-	private JRadioButton rb2;
-	private JSpinner jp;
-	private JLabel levelLabel;
-	private JLabel galaxyLabel;
-
-	private QAPInput input;
-	private QAPInputDetail qapinput;
-	private JButton refreshButton; 
-
+	
 	QAPOutputView(AbstractControllerView c) {
 		super(c);	
 	}
@@ -41,43 +30,14 @@ public class QAPOutputView extends ViewForm {
 	 * 
 	 */
 	public void showQAPInputForm(){
-		qapinput = new QAPInputDetail((QAPInputControllerView)controller);
-		qapinput.setVisible(true);
-		((QAPInputControllerView)controller).create_form_add();	
+		
 	}
 
 	/**
 	 * 
 	 */
 	protected void create_events() {
-		refreshButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e){
-				//refreshComboBox();
-			}
-		});
-
-		rb1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e){
-				Console.print("LAA 1!");
-				rb1.setSelected(true);
-				rb2.setSelected(false);
-
-			}
-		});
-
-		//Boton de eliminar
-		rb2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e){
-				rb1.setSelected(false);
-				rb2.setSelected(true);
-			}
-		});
-
-		inputButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e){
-				showQAPInputForm();
-			}
-		});
+	
 
 	}
 
@@ -94,87 +54,7 @@ public class QAPOutputView extends ViewForm {
 	 * 
 	 */
 	protected void create_view() {
-		galaxyLabel = new JLabel("SOY LA VISTA DE OUTPUT :D");
-		String[] nulo = new String[1];
-		nulo[0] = "None";
-		gcb = new JComboBox(nulo);
-		inputButton = new JButton("Create Input Form");
-		levelLabel = new JLabel("Set Level (Value -1: Infinite for Lazy and None for Eager)");
-		refreshButton = new JButton("Refresh Galaxies");
 		
-
-		galaxyLabel = new JLabel("Select Galaxy for Algorithm:");
-		rb1 = new JRadioButton("QAP Lazy Branch and Bound",true);
-		rb2 = new JRadioButton("QAP Eager Branch and Bound",false);
-
-
-
-		SpinnerNumberModel jspin = new SpinnerNumberModel(-1, -1, Integer.MAX_VALUE, 1);
-		jp = new JSpinner(jspin);
-
-		GroupLayout layout = new GroupLayout(this);
-		this.setLayout(layout);
-		layout.setVerticalGroup(
-				layout.createParallelGroup()
-					.addGroup(
-							layout.createSequentialGroup()
-								.addComponent(galaxyLabel,javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-								.addGroup(
-										layout.createSequentialGroup()
-										.addComponent(refreshButton)
-										.addComponent(gcb, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-								)
-								.addComponent(rb1)
-								.addGap(10)
-								.addComponent(rb2) 
-								.addGap(10)
-								.addComponent(levelLabel) 
-								.addGap(10)
-								.addComponent(jp, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE) 
-								.addGap(10)
-								.addComponent(inputButton)
-					)
-
-		);
-
-		layout.setHorizontalGroup(
-				layout.createParallelGroup()
-						.addGroup(
-								layout.createSequentialGroup()
-								.addComponent(galaxyLabel,javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE)
-						)
-						.addGroup(
-								layout.createSequentialGroup()
-								.addComponent(refreshButton)
-
-						)
-						.addGroup(
-								layout.createSequentialGroup()
-								.addComponent(gcb,javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-						
-						)
-						.addGroup(
-								layout.createSequentialGroup()
-								.addComponent(rb1)
-						)
-						.addGroup(
-								layout.createSequentialGroup()
-								.addComponent(rb2)
-						)
-						.addGroup(
-								layout.createSequentialGroup()
-								.addComponent(levelLabel)
-						)
-						.addGroup(
-								layout.createSequentialGroup()
-								.addComponent(jp,javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-						)
-						.addGroup(
-								layout.createSequentialGroup()
-								.addComponent(inputButton)
-						)
-
-		);
 	}
 
 }
